a505c068abcc7943c435bf408837525c0bbb42d8
aleksa-oss <kushitashvilialeksandre@gmail.com>
2025-07-20 14:34:16 -0400

    Fixed command line argument parsing

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import queue
+import socket
 
 def list_builder(max_num):
 	"""Build a queue containing numbers from 1 up to max_num inclusive."""
@@ -6,3 +7,11 @@ def list_builder(max_num):
 	for i in range(1, max_num + 1):
 		q.put(i)
 	return q
+	
+def is_valid_host(host):
+	"""Check if the host is a valid IP address or resolvable domain name."""
+	try:
+		socket.gethostbyname(host)
+		return True
+	except socket.error:
+		return False
